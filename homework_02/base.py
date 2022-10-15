52ea46458bc0cab61536a8eff40e90d94d372a0d
Max Rubets <rubets.me@gmail.com>
2022-10-15 20:27:43 +0300

    working with Vehicle

---
+++ homework_02/base.py
@@ -0,0 +1,26 @@
+from abc import ABC
+from homework_02 import exceptions
+
+
+class Vehicle(ABC):
+
+    def __init__(self, weight=5, started=True, fuel=60, fuel_consumption=2):
+        self.weight = weight
+        self.started = started
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.started :
+            pass
+        else :
+            if self.fuel <= 0:
+                raise exceptions.LowFuelError
+            else :
+                self.started = True
+
+    def move(self, dist : int):
+        if self.fuel < self.fuel_consumption * dist:
+            raise exceptions.NotEnoughFuel
+        else:
+            self.fuel -= self.fuel_consumption * dist
