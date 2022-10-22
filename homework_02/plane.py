45a80849f024e73db7015da6d5d1d9ee072c1b8c
Max Rubets <114773838+mxx-s@users.noreply.github.com>
2022-10-22 11:30:44 +0300

    test inti: set super() instead of vehicle

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -11,8 +11,8 @@ class Plane(Vehicle):
     max_cargo: int
 
     def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption)
         self.max_cargo = max_cargo
-        Vehicle().__init__(weight, fuel, fuel_consumption)
 
     def load_cargo(self, value : int):
         if self.max_cargo < self.cargo + value:
